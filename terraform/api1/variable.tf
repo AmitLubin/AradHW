9649c826db0ad2a5e48f0081bfd9f160b710faf8
AmitLubin <amit.lubin@gmail.com>
2025-05-11 12:16:11 +0300

    Added api gateway to lambda with cognito via terraform

---
+++ terraform/api1/variable.tf
@@ -0,0 +1,11 @@
+variable "api_id" {
+  default = "mj92zct6nc"
+}
+
+variable "region" {
+  default = "il-central-1"
+}
+
+variable "cognito_user_pool_id" {
+    default = "il-central-1_msZKXt9Ju"
+}
